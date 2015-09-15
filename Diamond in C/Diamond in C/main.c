f4867700906486831836ea1cdedd5a3845d434e9
Nohface <noah_armsworthy@hotmail.com>
2015-09-15 12:23:47 -0300

    Diamond in c main file created with some code

---
+++ Diamond in C/Diamond in C/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+	//Declare our variables
+	int diamondSize = 15;
+	int center = diamondSize / 2;
+	int spaces = center;
+	int os = 1;
+
+	printf("%i", center);
+
+
+
+
+
+
+	return 0;
+}
