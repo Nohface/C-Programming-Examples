a56547d09beadcee6416e9bafc280d8472173b21
Nohface <noah_armsworthy@hotmail.com>
2015-09-18 14:11:03 -0300

    Finished the diamond in c

--- Diamond in C/Diamond in C/main.c
+++ Diamond in C/Diamond in C/main.c
@@ -7,9 +7,40 @@ int main()
 	int spaces = center;
 	int os = 1;
 
-	printf("%i", center);
+	while (spaces >= 1)
+	{
+		for (int x = 1; x <= spaces; x++)
+		{
+			printf(" ");
+		}
 
+		for (int y = 1; y <= os; y++)
+		{
+			printf("o");
+		}
+		
+		spaces--;
+		os += 2;
+		printf("\n");
+	}
+	
+	while (spaces <= center)
+	{
+		for (int x = 1; x <= spaces; x++)
+		{
+			printf(" ");
+		}
 
+		for (int y = 1; y <= os; y++)
+		{
+			printf("o");
+		}
+
+		spaces++;
+		os -= 2;
+		printf("\n");
+	}
+	
 
 
 
