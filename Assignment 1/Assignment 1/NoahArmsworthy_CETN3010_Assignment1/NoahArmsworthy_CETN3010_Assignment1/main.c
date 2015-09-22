ad9edc84af697a247de406caf8d521a4ba484dd2
Nohface <noah_armsworthy@hotmail.com>
2015-09-22 12:13:15 -0300

    Added starting text and began letter switch

---
+++ Assignment 1/Assignment 1/NoahArmsworthy_CETN3010_Assignment1/NoahArmsworthy_CETN3010_Assignment1/main.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int number;
+	char letter;
+
+	printf("Hello. This is a simple 'console' calculator.\n");
+	printf("Simply enter the number followed by the operator that you wish to use.\n");
+	printf("It is possible to use the standard operators (+, -, *, / ) as well as two extra operators : \n");
+	printf("1) S, which set the accumulator; and\n");
+	printf("2) N, that ends the calculation (N.B. Must place a zero before N).\n");
+	printf("\n");
+	
+	
+	printf("Please enter a number and an operator: ");
+	scanf("%i%c", &number, &letter);
+
+	switch (letter)
+	{
+	case '+':;
+	case '-':;
+	case '*':;
+	case '/':;
+	}
+	
+	return 0;
+}
