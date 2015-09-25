52ea61609578c9426ca76352bf068d95966387f0
Nohface <flexible.harlequin@hotmail.com>
2015-09-25 15:31:02 -0300

    I think i'm done...

--- Assignment 1/Assignment 1/NoahArmsworthy_CETN3010_Assignment1/NoahArmsworthy_CETN3010_Assignment1/main.c
+++ Assignment 1/Assignment 1/NoahArmsworthy_CETN3010_Assignment1/NoahArmsworthy_CETN3010_Assignment1/main.c
@@ -1,13 +1,16 @@
+//define error cancellation and include necessary libraries
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
 
+//include main function
 int main()
 {
+	//declare variables
 	float number;
 	float total = 0;
 	char letter;
 
+	//print welcome statement and instructions
 	printf("Hello. This is a simple 'console' calculator.\n");
 	printf("Simply enter the number followed by the operator that you wish to use.\n");
 	printf("It is possible to use the standard operators (+, -, *, / ) as well as two extra operators : \n");
@@ -16,38 +19,60 @@ int main()
 	printf("\n");
 	
 	
-	
+	//do function while N is not typed
 	do
 	{
+		//ask for variable
 		printf("Please enter a number and an operator: ");
 		scanf("%f%c", &number, &letter);
-		printf("%f\n", total);
 		
+		//switch statement relying on the letter that follows the number
 		switch (letter)
 		{
 		case 'S':
+			//S sets the input number as the total to work with
 			(total = number);
+			break;
 		case 'N':
+			//recognizes if the use types '0N'
 			if (number == 0)
 			{
 				break;
 			}
 		case '+':
+			//adds the new number to the total
 			( total = total + number);
 			break;
 		case '-':
+			//subtracts the new number from the total
 			(total = total - number);
 			break;
 		case '*':
+			//multiplies the new number by the total
 			(total = total * number);
 			break;
 		case '/':
-			(total = total / number);
+			//as long as the new number is not 0, devide the total by the new number
+			if (number == 0)
+			{
+				printf("Cannot divide by zero.\n");
+				break;
+			}
+			else
+			{
+				(total = total / number);
+			}
 			break;
 		default: 
+			//have a default error text if you type an unrecognized letter
 			printf("Error. Please ensure you enter a correct number and operator.\n");
 		}
 		
+		//print the total only if the user has not typed N or tried to divide by 0
+		if ((letter != 'N' )&&(letter != '/' && number != 0))
+		{
+			printf("%f\n", total);
+		}
 	}while (letter != 'N');
 	
 	
