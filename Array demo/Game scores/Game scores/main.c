c8d89a08ce4ad512dc26a65560967a8d3918651a
Nohface <flexible.harlequin@hotmail.com>
2015-10-06 10:50:37 -0300

    Uploading for reference

---
+++ Array demo/Game scores/Game scores/main.c
@@ -0,0 +1,30 @@
+int main()
+{
+	int gameScores[10] = { 12, 5, 21, 15, 32, 10 };
+	int totalPoints = 0;
+	int i = 0;
+	float averagePoints;
+
+	int numberofElements = sizeof(gameScores) / sizeof(int);
+
+
+	for (i = 6; i < numberofElements; i++)
+	{
+		printf("What did the player scare in game %i?: ", i + 1);
+		scanf(" %i", &gameScores[i]);
+	}
+
+	
+	for (i = 0; i < numberofElements; i++)
+	{
+		totalPoints += gameScores[i];
+	}
+
+	averagePoints = totalPoints / numberofElements;
+
+	printf("The player's scoring average is %.1f points per game. \n", averagePoints);
+
+
+
+	return 0;
+}
