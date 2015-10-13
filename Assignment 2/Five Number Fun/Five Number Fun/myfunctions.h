5504f768f89cae9d38d12b85310344aedfbb4528
Nohface <flexible.harlequin@hotmail.com>
2015-10-13 12:21:09 -0300

    Noah Armsworthy_CETN_3010_Assignment 2

---
+++ Assignment 2/Five Number Fun/Five Number Fun/myfunctions.h
@@ -0,0 +1,5 @@
+float GetAverage(int numbers[], int size);
+void Sort(int numbers[], int size);
+int GetSum(int numbers[], int size);
+void PrintNumbers(int numbers[], int size);
+void PrintSumAsBinary(int numbers[], int size);
