65745d55cecb9992f53e5940b60ba0220203fd80
Nohface <flexible.harlequin@hotmail.com>
2015-10-27 11:35:33 -0300

    Finished function 3!

--- Assignment 3/Assignment 3/Assignment 3/main.c
+++ Assignment 3/Assignment 3/Assignment 3/main.c
@@ -1,3 +1,4 @@
+//include libraries
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdbool.h>
@@ -6,15 +7,18 @@
 
 int main()
 {
+	//declare variables
 	char command;
+	int start, length;
 	bool loopCon = true;
-	
-	char resultString[50] = { 0 };
+	//and arrays to store strings
+	char resultString[50];
 	char searchString[50];
-	char myString[50]= "Let us have some fun with strings!\n";
+	char myString[50]= "Let us have some fun with strings!\0";
 
-
+	//print string
 	printf("%s", myString);
+	printf("\n");;
 	
 	
 
@@ -29,38 +33,67 @@ int main()
 		printf("4 - Insert a word into the string\n");
 		printf("x - Exit the program\n");
 
+
+		//collect user input
 		printf("Your command:");
 		scanf(" %c", &command);
 
+		//switch command based on user input
 		switch (command)
 		{
 		case '1':
-			; int start, length;
+			//get starting point and length of the substring to extract
 			printf("Enter your starting point:");
 			scanf(" %i", &start);
 			printf("Enter your desired substring length:");
 			scanf(" %i", &length);
 			printf("\n");
 
+			//put those numbers in function and return the resulting substring
 			subString(myString, start, length, resultString);
 			puts(resultString);
 			break;
+
 		case '2':
+			//get word to locate
 			printf("\nEnter the word you'd like to find:");
 			scanf(" %[^\n]s", searchString);
+
+			//set variable for the int the function will return
 			int printIndex = findString(myString, searchString);
+
+			//print the number returned
 			printf("\nThe index position of your word is:%i", printIndex);
 			printf("\n");
 			break;
 		case '3':
+			printf("Enter your starting point:");
+			scanf(" %i", &start);
+			printf("Enter the length of characters to remove:");
+			scanf(" %i", &length);
+			printf("\n");
+
+			/*removeString(myString, resultString, start, length);
+			puts(myString);*/
+			removeString(myString, start, length);
+			puts(myString);
 			break;
 		case '4':
+			printf("\nEnter the word you'd like to insert:");
+			scanf(" %[^\n]s", searchString);
+			printf("Enter the starting point of insertion:");
+			scanf(" %i", &start);
+
+			insertString(myString, searchString, start);
+			puts(myString);
 			break;
 		case 'x':
+			//end the loop, print thank you text
 			loopCon = false;
 			printf("Thanks for playing!\n");
 			break;
 		default:
+			//print invalid message only if loop hasn't ended
 			if (loopCon != false)
 			{
 				printf("\nInvalid command. Try again.\n");
--- Assignment 3/Assignment 3/Assignment 3/myfunctions.c
+++ Assignment 3/Assignment 3/Assignment 3/myfunctions.c
@@ -1,9 +1,11 @@
 void subString(char source[], char start, int count, char result[])
 {
+	//set the position variable knowing that strings start at 0
 	int position = start - 1;
 	int j = 0;
 	int i = 0;
 
+	//loop at position as many times as position+count, taking letters from first array to second
 	for (i = position; i < position + count; i++, j++)
 	{
 		result[j] = source[i];
@@ -19,12 +21,15 @@ void subString(char source[], char start, int count, char result[])
 
 int findString(char source[], char stringToFind[])
 {
+	//set variables, including length of strings
 	int i = 0;
 	int j = 0;
 	int index = 0;
 	int len = strlen(source);
 	int len2 = strlen(stringToFind);
 
+	//search for two similar characters and check that it is the full length of the word
+	//return starting point of that word
 	while (len != 0)
 	{
 		if (source[i] == stringToFind[j])
@@ -36,9 +41,7 @@ int findString(char source[], char stringToFind[])
 				index = i - strlen(stringToFind) + 2;
 			}
 		}
-		
-		
-		
+
 		i++;
 		len--;
 		
@@ -50,8 +53,70 @@ int findString(char source[], char stringToFind[])
 
 
 
-void removeString(char source[], int startingIndex, int numberOfCharactersToRemove)
+void removeString(char source[], int startingIndex, int count)
 {
+	//declare variables
+	int i = 0;
+	int j = 0;
+	
+	//temporary array to store desired string
+	char result[50];
+	int position = startingIndex - 1;
+	int len = strlen(source);
+	int temp = position + count;
+
+	//do while the length of the source file doesn't = 0
+	do 
+	{
+		//while the index is less than the position
+		while (i < position)
+		{
+			result[j] = source[i];
+			j++;
+			i++;
+		}
+		
+		//and larger than the count
+		//add those characters to the result array
+		while (i > temp && i <= strlen(source))
+		{
+			result[j] = source[i];
+			
+			j++;
+			i++;
+		}
+
+		i++;
+		//decrease the length count of the source array each loop
+		len--;
+
+		//if the length is 0, add the null terminator
+		if (len == 0)
+			result[j] = '\0';
+
+	} while (len != 0);
+
+	
+	//get the length of the result array & set the variable back to 0
+	int len2 = strlen(result);
+	j = 0;
+	i = 0;
+
+	//while j is less than the length of result, replace the characters in the source
+	//with the ones from the result
+	while (j <= len2)
+	{
+		source[i] = result[j];
+		
+		//when you get to the end of the result array, add a null terminator to the source array
+		if (j == len2)
+			source[i] = '\0';
+		j++;
+		i++;
+	}
+		
+
+	
 
 }
 
